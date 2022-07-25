538b84d00afec235cd90fb3415449171396298ec
YuraSk <yura201093@gmail.com>
2022-07-25 23:56:18 +0300

    header wip

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import './App.css';
-import { useState } from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Flighs from './pages/Flights';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import UserProfile from './pages/UserProfile';
+import Header from './components/Header';
 
 function App() {  
   return (
-    <div className="App">
-      <h1>Book a flight!</h1>
-      <ul>
-        <li><Link to="/">Home</Link></li>
+    <>
+        {/* <li><Link to="/">Home</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">SignUp</Link></li>
         <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/book-flight">Book Flight</Link></li>
-      </ul>
-
+        <Link to="/book-flight">Book Flight</Link> */}
+      <Header />
       <Routes>
         {/* Declare all routes here: */}
         <Route exact path='/' element={ <Home /> }/ >
         <Route exact path='/login' element={ <Login/> } />
         <Route exact path='/signup' element={ <SignUp/> } />
         <Route exact path='/profile' element={ <UserProfile/> } />
-        <Route exact path='/book-flight' element={ <Flighs/> }/>
+        <Route exact path='/flights' element={ <Flighs/> }/>
       </Routes>
 
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+
+const drawerWidth = 240;
+const navItems = [{name: 'Home', path: '/'}, {name: 'Login', path: '/login'}, {name: 'Sign up', path: '/signup'}];
+
+function Header(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <Link href={item.path}><ListItemText primary={item.name} /></Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Flights Booking 
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Link href={item.path} key={item.name}>
+                <Button sx={{ color: '#fff' }}>
+                  {item.name}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
+
+export default Header;
